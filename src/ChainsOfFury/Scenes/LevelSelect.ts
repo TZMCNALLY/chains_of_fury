a6489c759e3e6434f278c66d9a5d778414e42bf8
Torin McNally <torinmcnally@Torins-MacBook-Pro.local>
2023-04-05 21:50:57 -0400

    Aaron's test

--- src/ChainsOfFury/Scenes/LevelSelect.ts
+++ src/ChainsOfFury/Scenes/LevelSelect.ts
@@ -60,12 +60,108 @@ export default class LevelSelect extends Scene {
 
 
         // Buttons:
-        let level1 = this.makeLevelBox(size.x-350, size.y-100, "level1");
-        let level2 = this.makeLevelBox(size.x, size.y-100, "level2");
-        let level3 = this.makeLevelBox(size.x+350, size.y-100, "level3");
-        let level4 = this.makeLevelBox(size.x-350, size.y+200, "level4");
-        let level5 = this.makeLevelBox(size.x, size.y+200, "level5");
-        let level6 = this.makeLevelBox(size.x+350, size.y+200, "level6");
+
+        let level1 = <Button>this.add.uiElement(
+            UIElementType.BUTTON,
+            MenuLayers.MAIN,
+            {
+                position: new Vec2(size.x-350, size.y-100),
+                text: "level1"
+            }
+        )
+        level1.backgroundColor = Color.WHITE;
+        level1.borderColor = Color.WHITE;
+        level1.borderRadius = 0;
+        level1.font = "PixelSimple";
+        level1.fontSize = 20;
+        level1.textColor = Color.BLACK;
+        level1.size.set(200, 200);
+
+        
+        let level2 = <Button>this.add.uiElement(
+            UIElementType.BUTTON,
+            MenuLayers.MAIN,
+            {
+                position: new Vec2(size.x, size.y-100),
+                text: "level2"
+            }
+        )
+        level2.backgroundColor = Color.WHITE;
+        level2.borderColor = Color.WHITE;
+        level2.borderRadius = 0;
+        level2.font = "PixelSimple";
+        level2.fontSize = 20;
+        level2.textColor = Color.BLACK;
+        level2.size.set(200, 200);
+
+
+        let level3 = <Button>this.add.uiElement(
+            UIElementType.BUTTON,
+            MenuLayers.MAIN,
+            {
+                position: new Vec2(size.x+350, size.y-100),
+                text: "level3"
+            }
+        )
+        level3.backgroundColor = Color.WHITE;
+        level3.borderColor = Color.WHITE;
+        level3.borderRadius = 0;
+        level3.font = "PixelSimple";
+        level3.fontSize = 20;
+        level3.textColor = Color.BLACK;
+        level3.size.set(200, 200);
+
+
+        let level4 = <Button>this.add.uiElement(
+            UIElementType.BUTTON,
+            MenuLayers.MAIN,
+            {
+                position: new Vec2(size.x-350, size.y+200),
+                text: "level4"
+            }
+        )
+        level4.backgroundColor = Color.WHITE;
+        level4.borderColor = Color.WHITE;
+        level4.borderRadius = 0;
+        level4.font = "PixelSimple";
+        level4.fontSize = 20;
+        level4.textColor = Color.BLACK;
+        level4.size.set(200, 200);
+
+
+        let level5 = <Button>this.add.uiElement(
+            UIElementType.BUTTON,
+            MenuLayers.MAIN,
+            {
+                position: new Vec2(size.x, size.y+200),
+                text: "level5"
+            }
+        )
+        level5.backgroundColor = Color.WHITE;
+        level5.borderColor = Color.WHITE;
+        level5.borderRadius = 0;
+        level5.font = "PixelSimple";
+        level5.fontSize = 20;
+        level5.textColor = Color.BLACK;
+        level5.size.set(200, 200);
+
+
+        let level6 = <Button>this.add.uiElement(
+            UIElementType.BUTTON,
+            MenuLayers.MAIN,
+            {
+                position: new Vec2(size.x+350, size.y+200),
+                text: "level6"
+            }
+        )
+        level6.backgroundColor = Color.WHITE;
+        level6.borderColor = Color.WHITE;
+        level6.borderRadius = 0;
+        level6.font = "PixelSimple";
+        level6.fontSize = 20;
+        level6.textColor = Color.BLACK;
+        level6.size.set(200, 200);
+
 
         // Scene transitions:
 
@@ -100,27 +196,5 @@ export default class LevelSelect extends Scene {
     public unloadScene(): void {
         // The scene is being destroyed, so we can stop playing the song
         // this.emitter.fireEvent(GameEventType.STOP_SOUND, {key: MainMenu.MUSIC_KEY});
-    }
-
-    // Creates a level box and appends it onto main layer.
-    // x and y are positions of the box
-    public makeLevelBox(x: number, y: number, txt: String): Button {
-        let levelBox = <Button>this.add.uiElement(
-            UIElementType.BUTTON,
-            MenuLayers.MAIN,
-            {
-                position: new Vec2(x, y),
-                text: txt
-            }
-        )
-        levelBox.backgroundColor = Color.BLACK;
-        levelBox.borderColor = Color.BLACK;
-        levelBox.borderRadius = 0;
-        levelBox.font = "PixelSimple";
-        levelBox.fontSize = 20;
-        levelBox.textColor = Color.RED;
-        levelBox.size.set(200, 200);
-
-        return levelBox;
     }
 }

4bddb44d31f29e0e73dcf4a7fe6a5d0d8a266685
Aaron Liang <aaron.liang@stonybrook.edu>
2023-04-05 20:41:57 -0400

    Controls and Help Pages + some code simplification

--- src/ChainOfFury/Scenes/MainMenu.ts
+++ src/ChainOfFury/Scenes/MainMenu.ts
@@ -6,6 +6,8 @@ import Scene from "../../Wolfie2D/Scene/Scene";
 import Color from "../../Wolfie2D/Utils/Color";
 import LevelSelect from "./LevelSelect";
 import SplashScreen from "./SplashScreen";
+import Help from "./Help";
+import Controls from "./Controls";
 
 // Layers for the main menu scene
 export const MenuLayers = {
@@ -44,78 +46,15 @@ export default class MainMenu extends Scene {
             }
         )
         title.fontSize = 35;
-        title.textColor = Color.WHITE;
+        title.textColor = Color.RED;
 
 
         // Buttons:
 
-        let startGame = <Button>this.add.uiElement(
-            UIElementType.BUTTON,
-            MenuLayers.MAIN,
-            {
-                position: new Vec2(size.x, size.y+20),
-                text: "Start Game"
-            }
-        )
-        startGame.backgroundColor = Color.WHITE;
-        startGame.borderColor = Color.WHITE;
-        startGame.borderRadius = 0;
-        startGame.font = "PixelSimple";
-        startGame.fontSize = 20;
-        startGame.textColor = Color.BLACK;
-        startGame.size.set(355, 35);
-
-        
-        let levelSelect = <Button>this.add.uiElement(
-            UIElementType.BUTTON,
-            MenuLayers.MAIN,
-            {
-                position: new Vec2(size.x, size.y+100),
-                text: "Level Select"
-            }
-        )
-        levelSelect.backgroundColor = Color.WHITE;
-        levelSelect.borderColor = Color.WHITE;
-        levelSelect.borderRadius = 0;
-        levelSelect.font = "PixelSimple";
-        levelSelect.fontSize = 20;
-        levelSelect.textColor = Color.BLACK;
-        levelSelect.size.set(355, 35);
-
-
-        let controls = <Button>this.add.uiElement(
-            UIElementType.BUTTON,
-            MenuLayers.MAIN,
-            {
-                position: new Vec2(size.x, size.y+180),
-                text: "Controls"
-            }
-        )
-        controls.backgroundColor = Color.WHITE;
-        controls.borderColor = Color.WHITE;
-        controls.borderRadius = 0;
-        controls.font = "PixelSimple";
-        controls.fontSize = 20;
-        controls.textColor = Color.BLACK;
-        controls.size.set(355, 35);
-
-
-        let help = <Button>this.add.uiElement(
-            UIElementType.BUTTON,
-            MenuLayers.MAIN,
-            {
-                position: new Vec2(size.x, size.y+260),
-                text: "Help"
-            }
-        )
-        help.backgroundColor = Color.WHITE;
-        help.borderColor = Color.WHITE;
-        help.borderRadius = 0;
-        help.font = "PixelSimple";
-        help.fontSize = 20;
-        help.textColor = Color.BLACK;
-        help.size.set(355, 35);
-
+        let startGame = this.createButton(size.x, size.y+20, "Start Game");
+        let levelSelect = this.createButton(size.x, size.y+100, "Level Select");
+        let controls = this.createButton(size.x, size.y+180, "Controls");
+        let help = this.createButton(size.x, size.y+260, "Help");
 
         // Scene transitions:
 
@@ -128,11 +67,11 @@ export default class MainMenu extends Scene {
         }
 
         controls.onClick = () => {
-            this.sceneManager.changeToScene(SplashScreen);
+            this.sceneManager.changeToScene(Controls);
         }
 
         help.onClick = () => {
-            this.sceneManager.changeToScene(SplashScreen);
+            this.sceneManager.changeToScene(Help);
         }
 
         // Scene has started, so start playing music
@@ -142,5 +81,27 @@ export default class MainMenu extends Scene {
     public unloadScene(): void {
         // The scene is being destroyed, so we can stop playing the song
         // this.emitter.fireEvent(GameEventType.STOP_SOUND, {key: MainMenu.MUSIC_KEY});
+    }
+
+    // Creates a button and appends it onto main layer.
+    // x and y are positions of the label
+    public createButton(x : number, y : number, txt : String): Button {
+        let button = <Button>this.add.uiElement(
+            UIElementType.BUTTON,
+            MenuLayers.MAIN,
+            {
+                position: new Vec2(x, y),
+                text: txt
+            }
+        )
+        button.backgroundColor = Color.BLACK;
+        button.borderColor = Color.BLACK;
+        button.borderRadius = 0;
+        button.font = "PixelSimple";
+        button.fontSize = 20;
+        button.textColor = Color.RED;
+        button.size.set(355, 35);
+
+        return button;
     }
 }

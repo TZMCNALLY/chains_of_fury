0a78220ab66a97768e3e7c7643b6f1733485badc
Vincent <yugui.ke@stonybrook.edu>
2023-04-20 14:01:46 -0400

    Change state change to use deltaT instead of Date

--- src/ChainsOfFury/Enemy/MoonDog/MoonDogStates/Idle.ts
+++ src/ChainsOfFury/Enemy/MoonDog/MoonDogStates/Idle.ts
@@ -6,28 +6,27 @@ import RandUtils from "../../../../Wolfie2D/Utils/RandUtils";
 
 export default class Idle extends MoonDogState {
 
-	protected lastActionTime: number = -1;
+	protected lastActionTime: number;
 
 	public onEnter(options: Record<string, any>): void {
         this.owner.animation.play(MoonDogAnimation.IDLE);
+
+		this.lastActionTime = 2; // How many seconds it takes until it goes into new state.
 	}
 
 	public update(deltaT: number): void {
         // Adjust the direction the player is facing
 		super.update(deltaT);
 
-		// if (this.lastActionTime == -1) {
-		// 	this.lastActionTime = new Date().getTime() / 1000;
-		// }
-
-		// if (new Date().getTime() / 1000 - this.lastActionTime > 1) {
-		// 	this.lastActionTime = new Date().getTime() / 1000;
-
-		// 	if (RandUtils.randInt(1, 101) < 51) {
-		// 		// 50% chance to start charging.
-		// 		this.finished(MoonDogStates.CHARGE);
-		// 	}
-		// }
+		if (this.lastActionTime < deltaT) {
+			if (RandUtils.randInt(1, 101) < 51) {
+				// 50% chance to start charging.
+				this.finished(MoonDogStates.CHARGE);
+			}
+		} else {
+			// Reduce time until next action.
+			this.lastActionTime -= deltaT;
+		}
 	}
 
 	public onExit(): Record<string, any> {

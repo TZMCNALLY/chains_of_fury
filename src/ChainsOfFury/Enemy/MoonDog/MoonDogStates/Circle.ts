960fd3b8772be557e94599ea62610506807de146
Vincent <yugui.ke@stonybrook.edu>
2023-04-22 14:58:41 -0400

    Draft structure for moondog states

--- src/ChainsOfFury/Enemy/MoonDog/MoonDogStates/Circle.ts
+++ src/ChainsOfFury/Enemy/MoonDog/MoonDogStates/Circle.ts
@@ -4,33 +4,36 @@ import MoonDogState from "./MoonDogState";
 import MoonDogController from "../MoonDogController";
 import RandUtils from "../../../../Wolfie2D/Utils/RandUtils";
 
-export default class Idle extends MoonDogState {
+export default class Circle extends MoonDogState {
 
 	protected lastActionTime: number;
 
 	public onEnter(options: Record<string, any>): void {
-        this.owner.animation.play(MoonDogAnimation.IDLE);
+        this.owner.animation.play(MoonDogAnimation.IDLE); // TODO: Change this.
 
 		this.lastActionTime = 2; // How many seconds it takes until it goes into new state.
-
-		this.owner.alpha = .1;
 	}
 
 	public update(deltaT: number): void {
         // Adjust the direction the player is facing
 		super.update(deltaT);
 
+        // Calculate new point for enemy to move to
+
+        // If distrance is large. Go faster
+
+        // Determien next action.
 		if (this.lastActionTime < deltaT) {
 			let rnd = RandUtils.randInt(1, 101);
 
-			if (this.parent.health > 300) {
-				// Stage 1
+			if (this.parent.health > 100) {
+				// Stage 2
 
 				if (rnd < 21) {
 					// 20% chance to try to use magic attack.
 				}
 			} else {
-				// Stage 2
+				// Final stage
 				
 				// Enter the circled state.
 				// Rest of boss behavior defined there.

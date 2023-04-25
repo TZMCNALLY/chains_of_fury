b2d105eecacf5216ae1ecce0fe92e623070eb9a7
Aaron Liang <98434199+DancinCocobean@users.noreply.github.com>
2023-04-25 17:31:12 -0400

    Reaper added

---
+++ src/ChainsOfFury/Enemy/Reaper/ReaperStates/Dead.ts
@@ -0,0 +1,24 @@
+import { COFEvents } from "../../../COFEvents";
+import ReaperState from "./ReaperState";
+import { ReaperAnimation } from "../ReaperController";
+
+export default class Dead extends ReaperState {
+
+	public onEnter(options: Record<string, any>): void {
+		this.owner.animation.play(ReaperAnimation.DYING, false, null);
+        this.owner.animation.queue(ReaperAnimation.DEAD);
+	}
+
+	public update(deltaT: number): void {
+		super.update(deltaT);
+		if (!this.owner.animation.isPlaying(ReaperAnimation.DYING) && 
+        !this.owner.animation.isPlaying(ReaperAnimation.DEAD)) {
+            this.emitter.fireEvent(COFEvents.BOSS_DEFEATED);
+        }
+	}
+
+	public onExit(): Record<string, any> {
+		this.owner.animation.stop();
+		return {};
+	}
+}
---
+++ src/ChainsOfFury/Enemy/Reaper/ReaperStates/Walk.ts
@@ -0,0 +1,41 @@
+import Vec2 from "../../../../Wolfie2D/DataTypes/Vec2";
+import Idle from "./Idle";
+import ReaperState from "./ReaperState";
+import { ReaperAnimation } from "../ReaperController";
+
+export default class Walk extends ReaperState {
+
+	public onEnter(options: Record<string, any>): void {
+        this.parent.speed = 20;
+		if (this.parent.getXDistanceFromPlayer() > 0)
+        	this.owner.animation.play(ReaperAnimation.WALK_LEFT);
+		else
+        	this.owner.animation.play(ReaperAnimation.WALK_RIGHT);
+	}
+
+	public update(deltaT: number): void {
+		super.update(deltaT);
+
+		// Set appropriate player direction
+		if (this.parent.getXDistanceFromPlayer() < 0) {
+			this.faceXDir = 1
+			this.owner.animation.playIfNotAlready(ReaperAnimation.WALK_RIGHT);
+		} else {
+			this.faceXDir = -1
+			this.owner.animation.playIfNotAlready(ReaperAnimation.WALK_LEFT);
+		}
+		if (this.parent.getYDistanceFromPlayer() < 0) {
+        	this.faceYDir = 1
+		} else {
+			this.faceYDir = -1
+		}
+
+		// Move reaper towards player
+        this.owner.move(new Vec2(this.faceXDir, this.faceYDir).scale(this.parent.speed).scale(deltaT));
+	}
+
+	public onExit(): Record<string, any> {
+		this.owner.animation.stop();
+		return {};
+	}
+}

4d3d3d1ac0be05be24968df7a933965597a84a4f
Vincent <yugui.ke@stonybrook.edu>
2023-05-01 16:00:14 -0400

    Object pool for second boss

--- src/ChainsOfFury/Scenes/COFLevel2.ts
+++ src/ChainsOfFury/Scenes/COFLevel2.ts
@@ -1,4 +1,5 @@
 import COFLevel from "./COFLevel";
+import { COFLayers } from "./COFLevel";
 import COFLevel5 from "./COFLevel5"
 import AnimatedSprite from "../../Wolfie2D/Nodes/Sprites/AnimatedSprite";
 import Input from "../../Wolfie2D/Input/Input";
@@ -7,8 +8,15 @@ import EnemyController from "../Enemy/EnemyController";
 import AI from "../../Wolfie2D/DataTypes/Interfaces/AI";
 import DarkStalkerController from "../Enemy/DarkStalker copy/DealthStalkerStates/DarkStalkerController";
 import COFLevel3 from "./COFLevel3";
+import { COFPhysicsGroups } from "../COFPhysicsGroups";
 
 export default class COFLevel2 extends COFLevel {
+
+    /** Objects pools for various attacks */
+    private mineBalls: Array<AnimatedSprite>;
+    private missles: Array<AnimatedSprite>;
+    private eyeBalls: Array<AnimatedSprite>;
+
 
     /**
      * @see Scene.update()
@@ -28,5 +36,39 @@ export default class COFLevel2 extends COFLevel {
     protected handleLevelEnd(): void {
         super.handleLevelEnd();
         this.sceneManager.changeToScene(COFLevel3)
+    }
+
+    protected initObjectPools(): void {
+        super.initObjectPools();
+
+        this.mineBalls = new Array(12);
+        this.missles = new Array(4);
+        this.eyeBalls = new Array(4);
+
+        /** mineBalls initialization */
+        for (let i = 0; i < 12; i++) {
+            // TODO make a sprite for this!!!
+            this.mineBalls[i] = this.add.animatedSprite("fireball", COFLayers.PRIMARY);
+
+            this.mineBalls[i].visible = false; // Turns them off.
+
+            // this.mineBalls[i].addAI(); // Adds an AI to them..
+
+            // This will have no group.
+            // When the mine activates, it will have the contact damage group!
+        }
+
+        /** Missles and eyeBall initialization */
+        for (let i = 0; i < 4; i++) {
+            // TODO make a sprite for them!!
+
+            this.missles[i] = this.add.animatedSprite("fireball", COFLayers.PRIMARY);
+            this.eyeBalls[i] = this.add.animatedSprite("fireball", COFLayers.PRIMARY);
+
+            // TODO add AI for both of them.
+
+            // Only missles will have ENEMY_PROJECTILE group.
+            this.missles[i].setGroup(COFPhysicsGroups.ENEMY_PROJECTILE);
+        }
     }
 }

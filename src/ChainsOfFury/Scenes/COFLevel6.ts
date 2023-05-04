6329c6402c5178d9818b7bdc8a588e5fee43a271
TZMCNALLY <torin.mcnally@stonybrook.edu>
2023-05-04 18:42:02 -0400

    Lightning strike added

--- src/ChainsOfFury/Scenes/COFLevel6.ts
+++ src/ChainsOfFury/Scenes/COFLevel6.ts
@@ -8,7 +8,8 @@ import MainMenu from "./MainMenu";
 import GameEvent from '../../Wolfie2D/Events/GameEvent';
 import { COFEvents } from '../COFEvents';
 import { DemonKingEvents } from "../Enemy/DemonKing/DemonKingStates/DemonKingEvents";
-import Vec2 from "../../Wolfie2D/DataTypes/Vec2";
+import Vec2 from '../../Wolfie2D/DataTypes/Vec2';
+import AABB from "../../Wolfie2D/DataTypes/Shapes/AABB";
 
 export default class COFLevel6 extends COFLevel {
 
@@ -38,7 +39,8 @@ export default class COFLevel6 extends COFLevel {
         super.handleEvent(event);
         switch (event.type) {
             case DemonKingEvents.STRUCK_LIGHTNING: {
-                this.spawnLightningStrike();
+                this.spawnLightningStrike(event.data.get("location"));
+                this.handleLightningStrike(event.data.get("location"));
                 break;
             }
             case COFEvents.BOSS_DEFEATED: {
@@ -52,20 +54,26 @@ export default class COFLevel6 extends COFLevel {
         }
     }
 
-    protected spawnLightningStrike(): void {
-
-        this.lightningStrike.position.copy(new Vec2(this.player.position.x, this.player.position.y))
+    protected spawnLightningStrike(location: Vec2): void {
+        this.lightningStrike.position.copy(location)
         this.lightningStrike.visible = true;
         this.lightningStrike.animation.play("STRIKE", false, DemonKingEvents.LIGHTNING_STRIKE_ENDED)
     }
 
     protected despawnLightningStrike(): void { this.lightningStrike.visible = false; }
 
+    protected handleLightningStrike(location: Vec2): void {
+        let strikeHitbox = new Vec2(5, 5)
+        if (this.player.collisionShape.overlaps(new AABB(location, strikeHitbox))) {
+            this.emitter.fireEvent(COFEvents.ENEMY_PROJECTILE_HIT_PLAYER);
+        }
+    }
+
     protected initObjectPools(): void {
         super.initObjectPools();
 
         this.lightningStrike = this.add.animatedSprite("lightning_strike", COFLayers.PRIMARY);
-        this.lightningStrike.scale.set(6, 10)
+        this.lightningStrike.scale.set(1,9)
         this.lightningStrike.visible = false;
     }
 
